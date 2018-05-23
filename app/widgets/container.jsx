66954266042b05091b7875743016bf589c80949c
Jordi Moraleda <ledfusion@users.noreply.github.com>
2018-05-23 18:57:19 +0200

    Update container.jsx

--- app/widgets/container.jsx
+++ app/widgets/container.jsx
@@ -11,22 +11,22 @@ const Container = ({ children, xs, sm, md, lg, xl, className, ...props }) => (
 					}` : ""};
 					@media (min-width: 576px) {
 						.container {
-							max-width: ${sm || xs || 560}px;
+							max-width: ${sm || (xs != Container.defaultProps.xs && xs) || 560}px;
 						}
 					}
 					@media (min-width: 768px) {
 						.container {
-							max-width: ${md || sm || xs || 750}px;
+							max-width: ${md || sm || (xs != Container.defaultProps.xs && xs) || 750}px;
 						}
 					}
 					@media (min-width: 992px) {
 						.container {
-							max-width: ${lg || md || sm || xs || 920}px;
+							max-width: ${lg || md || sm || (xs != Container.defaultProps.xs && xs) || 920}px;
 						}
 					}
 					@media (min-width: 1200px) {
 						.container {
-							max-width: ${xl || lg || md || sm || xs || 980}px;
+							max-width: ${xl || lg || md || sm || (xs != Container.defaultProps.xs && xs) || 980}px;
 						}
 					}
 				`.replace(/[ \t\n]/g, "")}
@@ -38,11 +38,14 @@ const Container = ({ children, xs, sm, md, lg, xl, className, ...props }) => (
 Container.propTypes = {
 	className: PropTypes.string,
 	children: PropTypes.any,
-	xs: PropTypes.number,
-	sm: PropTypes.number,
+	xs: PropTypes.number, // max width on XS
+	sm: PropTypes.number, // ...
 	md: PropTypes.number,
 	lg: PropTypes.number,
 	xl: PropTypes.number
 };
+Container.defaultProps = {
+	xs: 560
+}
 
 export default Container;
